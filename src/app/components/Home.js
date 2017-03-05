981025acc64c0a3d48277769ab07d6fc1fe565d9
Blake <lovelace.b@hotmail.com>
2017-03-05 22:28:09 +0800

    passing data from parent tags to child tags and vice versa complete

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -4,19 +4,30 @@ export class Home extends React.Component {
 	constructor(props) {
 		super();
 		this.state ={
-			age: props.initialAge
-		}
+			age: props.initialAge,
+			status: 0,
+			homeLink: "Changed Link"
+		};
+
+		setTimeout(() => {
+			this.setState({status: 1 });
+		},1000);
 	}
 
 	onMakeOlder() {
 		this.setState({age: this.state.age + 3});
 	}
 
+	onChangeLink() {
+		this.props.changeLink(this.state.homeLink);
+	}
+
 	render() {
 		return (
 			<div>
 				<p>Your name is {this.props.name}!</p>
-				<p>You are {this.state.age} years old</p>	
+				<p>You are {this.state.age} years old</p>
+				<p>DOM Status!! {this.state.status}</p>
 				<p> User Object name = Name: {this.props.user.name}</p>
 				<div>
 					<h4>Hobbies</h4>
@@ -28,6 +39,12 @@ export class Home extends React.Component {
 					{this.props.children}
 				<hr/>
 				<button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older</button>
+				<hr/>
+				<button onClick={this.props.greet} className="btn btn-primary">
+					Greet
+				</button>
+				<hr/>
+				<button onClick={this.onChangeLink.bind(this)} className="btn btn-primary">Change Header Link</button>
 			</div>
 		);
 	}
@@ -37,5 +54,6 @@ Home.propTypes = {
 	name: React.PropTypes.string,
 	age: React.PropTypes.number,
 	user: React.PropTypes.object,
-	children: React.PropTypes.element.isRequired
+	children: React.PropTypes.element.isRequired,
+	greet: React.PropTypes.func
 };
--- src/app/index.js
+++ src/app/index.js
@@ -5,22 +5,43 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component {
+	constructor(){
+		super();
+
+		this.state={
+			homeLink: "Home"
+		};
+	}
+
+	onGreet(){
+		alert("Hello");
+	}
+
+	onChangeLinkName(newName) {
+		alert(newName);
+		this.setState({homeLink: newName});
+	}
+
 	render() {
 		var user = {
 			name: "Anna",
 			hobbies: ["Sports", "Reading"]
 		};
-
 		return (
 			<div className="container">
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Header/>
+						<Header homeLink={this.state.homeLink}/>
 					</div>
 				</div>
 				<div className="row">
 					<div className="col-xs-10 col-xs-offset-1">
-						<Home name={"max"} initialAge={23} user={user}>
+						<Home
+							name={"Max"}
+							initialAge={23}
+							user={user}
+							greet={this.onGreet}
+							changeLink={this.onChangeLinkName.bind(this)}>
 							<p>This is a paragraph inside the home tag</p>
 						</Home>
 					</div>
